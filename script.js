0ecb5d240119e8dac9f4b4a63dbdbcb5d7d5c88b
Sela Privette <selaprivette@yahoo.com>
2022-05-31 20:56:36 -0400

    added comments

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var currentTime = moment().hour();
 var avaliableTime = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 var saveBtn = document.querySelectorAll(".saveBtn");
-//schedule changes color depending on time of day//
+//schedule changes color depending on time of day, pushes to local storage//
 document.querySelector("#currentDay").textContent = moment().format("dddd, MMMM Do YYYY");
 for (var i = 0; i < 9; i++) {
     if (currentTime === avaliableTime[i]) {
